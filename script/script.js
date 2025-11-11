822708939b85a373e0cf34245b6d738425db8445
DieudyMuhunga <dieudymhg@gmail.com>
2025-11-11 21:44:26 +0100

    new commit

--- script/script.js
+++ script/script.js
@@ -4,3 +4,4 @@ const Menu = document.getElementById("menu");
 document.getElementById("menu-toggle").addEventListener("click", function() {
     document.getElementById("menu").classList.toggle("open");
 } );
+
