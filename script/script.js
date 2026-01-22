257c7e64b349475720cbbe782b3d9899e08d46bf
DieudyMuhunga <dieudymhg@gmail.com>
2026-01-22 19:33:27 +0100

    Mise en page

--- script/script.js
+++ script/script.js
@@ -1,7 +1,10 @@
-const toggleButton= document.getElementById("menu-toggle");
-const Menu = document.getElementById("menu");
+const links = document.querySelectorAll('nav ul li a');
 
-document.getElementById("menu-toggle").addEventListener("click", function() {
-    document.getElementById("menu").classList.toggle("open");
-} );
-
+for (const link of links) {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+    });
+}
